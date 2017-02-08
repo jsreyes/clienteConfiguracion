3c508d031beb7c3ae2c6d9e3dea772a4f002e270
jsreyes <ingjsreyesm@gmail.com>
2017-02-08 18:45:07 -0500

    Se ajusta que cuando un perfil no tenga menús asociados, quedaba la variable dataForTree en null, y por esta razón al seleccionar otros perfiles para ver sus menús asociados no los pintaba, además se configuraron las alertas de asociar un nuevo menú al perfil y la alerta de restricción cuando ya este asociado

--- app/scripts/controllers/consultarperfil.js
+++ app/scripts/controllers/consultarperfil.js
@@ -36,14 +36,14 @@ angular.module('javierApp')
    }; 
 
   //Función que arma la tabla  
-	$scope.gridOptions1 = {
+  $scope.gridOptions1 = {
         enableSorting: true,
         enableFiltering: true,
         columnDefs: [
           { field: 'Nombre',  cellTemplate: tmpl },
-          { field: 'Aplicacion.Nombre',  displayName: 'Aplicación',  cellTemplate: tmpl},
+          { field: 'Aplicacion.Nombre',  displayName: 'Aplicación',  enableCellEdit: false, cellTemplate: tmpl},
           { field: 'Acciones', 
-          cellTemplate: '<button class="btn btn-danger btn-circle" ng-click="grid.appScope.deleteRow(row)" type="submit"><i class="glyphicon glyphicon-trash"></i></button>&nbsp;<button type="button" class="btn btn-success btn-circle" ng-click="grid.appScope.actualizar(row); change_state()" ng-show="on_off"><i class="glyphicon glyphicon-pencil"></i></button>&nbsp;<button type="button" class="btn btn-success btn-circle" ng-click="grid.appScope.actualizar(row);change_state()" ng-hide="on_off"><i class="glyphicon glyphicon-ok"></i></button>&nbsp;<button type="button" class="btn btn-primary btn-circle" ng-click="grid.appScope.visualizar(row);grid.appScope.showAdvanced($event, row)" data-toggle="modal" data-target="#exampleModalLong"><i class="glyphicon glyphicon-cog"></i></button>'}
+          cellTemplate: '<button class="btn btn-danger btn-circle" ng-click="grid.appScope.deleteRow(row)" type="submit"><i class="glyphicon glyphicon-trash"></i></button>&nbsp;<button type="button" class="btn btn-success btn-circle" ng-click="grid.appScope.actualizar(row); change_state()" ng-show="on_off"><i class="glyphicon glyphicon-pencil"></i></button>&nbsp;<button type="button" class="btn btn-success btn-circle" ng-click="grid.appScope.actualizar(row);change_state()" ng-hide="on_off"><i class="glyphicon glyphicon-pencil"></i></button>&nbsp;<button type="button" class="btn btn-primary btn-circle" ng-click="grid.appScope.visualizar(row);grid.appScope.showAdvanced($event, row)" data-toggle="modal" data-target="#exampleModalLong"><i class="glyphicon glyphicon-eye-open"></i></button>'}
         ],
 
       };
@@ -102,12 +102,15 @@ angular.module('javierApp')
 
           console.log(row.entity.Id);
           $scope.prueba = row.entity;
-          console.log(row.entity.Aplicacion.Id);
+          console.log(row.entity.Nombre);
 
           //Obtiene los menús asociados a ese perfil
          $http.get('http://127.0.0.1:8081/v1/menu_opcion_padre/ArbolMenus/'+row.entity.Nombre+'')
             .then(function(response) {
                 $scope.opciones = response.data;
+                if ($scope.opciones === null) {
+                    $scope.opciones = {};
+                };
                 console.log($scope.opciones);
                 //$scope.showComplex();
               })
@@ -117,6 +120,7 @@ angular.module('javierApp')
 
           //Carga los menus por aplicación
           $http.get('http://127.0.0.1:8081/v1/perfil_x_menu_opcion/MenusPorAplicacion/' + row.entity.Aplicacion.Id +'').then(function(response) {
+            //Le asigna la respuesta de la petición a la variable
             $scope.dataForTheTree = response.data;
             });
       };
@@ -127,13 +131,67 @@ angular.module('javierApp')
               //For para realizar el post a la tabla perfil_x_menu_opcion
               for(var i = 0; i < $scope.nuevo_menu_x_perfil.length; i++){
 
+
                 //Se realiza la petición POST, para guardar los menús asociados al perfil
                 $http.post('http://127.0.0.1:8081/v1/perfil_x_menu_opcion/', {"Perfil": $scope.prueba, "Opcion" :  $scope.nuevo_menu_x_perfil[i]})
                   .then(function(response){
-                    console.log(response.data);
-                    alert("Los nuevos menús asociados al perfil, se han guardado satisfactoriamente");
+                     //Condicional
+                    if (response.data === "pq: llave duplicada viola restricción de unicidad «uq_perfil_x_menu_opcion_perfil_opcion»"){
+                          
+                          $scope.nombre_menu = response.config.data.Opcion.Nombre;
+                          console.log(response.config.data.Opcion.Nombre);
+
+                          alert("El menú " + $scope.nombre_menu +" ya se encuentra asociado al perfil");
+
+                    }else{
+                      $scope.nombre_menu = response.config.data.Opcion.Nombre;
+
+                      alert("El nuevo menú " + $scope.nombre_menu + " se ha asociado al perfil satisfactoriamente");
+
+                      //Cargar los nuevos menús asociados
+                      //Obtiene los menús asociados a ese perfil
+                      $http.get('http://127.0.0.1:8081/v1/menu_opcion_padre/ArbolMenus/'+$scope.prueba.Nombre+'')
+                      .then(function(response) {
+                        $scope.opciones = response.data;
+                          if ($scope.opciones === null) {
+                              $scope.opciones = {};
+                          };
+                             
+                      })
+
+                    }                    
                 });
 
+              }  
+
+                console.log($scope.nuevo_menu_x_perfil);
+
+      };  
+
+      //Función para guardar nuevos menus al perfil
+       $scope.borrar = function(){
+        console.log("Entro a borrar");
+
+              //For para realizar el post a la tabla perfil_x_menu_opcion
+              for(var i = 0; i < $scope.menu_x_perfil.length; i++){
+
+                console.log($scope.menu_x_perfil[i].Id);
+
+                //Se realiza la petición POST, para guardar los menús asociados al perfil
+                $http.delete('http://127.0.0.1:8081/v1/perfil_x_menu_opcion/' + $scope.menu_x_perfil[i].Id)
+                  .then(function(response){
+                    console.log(response.data);
+                    //Condicional
+                     if (response.data === "OK"){
+                           //$scope.gridOptions1.data.splice(index, 1); Sirve para hacer el borrado desde la vista
+                           alert("El menú asociado se ha borrado exitosamente");
+    
+                     }else{
+                           alert("No se puede borrar el menú asociado");
+                     }  
+                });
+
+                
               }   
       };           
     
